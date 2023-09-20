f41f9ddf2c721a7c8d496b8a1e0d9a99154a38d7
Tobias Bergstedt <info@tobiasbergstedt.se>
2023-09-20 10:34:20 +0200

    Update Article funtionality and styling.

--- src/pages/Start/Article/Article.tsx
+++ src/pages/Start/Article/Article.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next';
+import clsx from 'clsx';
 
 import Button from '../../../components/Button/Button';
 
@@ -8,21 +9,50 @@ interface ArticleProps {
   title: string;
   description: string;
   linkTo: string;
+  isMainArticle?: boolean;
+  imageUrl?: string;
+  imageAlt?: string;
 }
 
-const Article = ({ title, description, linkTo }: ArticleProps): JSX.Element => {
+const Article = ({
+  title,
+  description,
+  linkTo,
+  isMainArticle,
+  imageUrl,
+  imageAlt,
+}: ArticleProps): JSX.Element => {
   const { t } = useTranslation();
 
   return (
     <article className={styles.mainArticle}>
-      <section className={styles.mainContent}>
-        <h1 className={styles.mainHeading}>{title}</h1>
-        <p className={styles.mainParagraph}>{description}</p>
+      <section
+        className={clsx(styles.mainContent, {
+          [styles.gridContent]: !(isMainArticle ?? false),
+        })}
+      >
+        {isMainArticle === true ? (
+          <h1 className={styles.mainHeading}>{title}</h1>
+        ) : (
+          <h2 className={clsx(styles.mainHeading, styles.smallerHeading)}>
+            {title}
+          </h2>
+        )}
+
+        <p
+          className={clsx(styles.mainParagraph, {
+            [styles.smallParagraph]: !(isMainArticle ?? false),
+          })}
+        >
+          {description}
+        </p>
         <Button to={linkTo}>{t('start.readMore')}</Button>
       </section>
-      <div className={styles.mainImageWrapper}>
-        <img className={styles.mainImage}></img>
-      </div>
+      {imageUrl != null && (
+        <div className={styles.mainImageWrapper}>
+          <img src={imageUrl} alt={imageAlt} className={styles.mainImage} />
+        </div>
+      )}
     </article>
   );
 };
