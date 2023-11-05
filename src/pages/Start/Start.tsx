07572f20096779593f5004127703b55c8bd74c77
Tobias Bergstedt <info@tobiasbergstedt.se>
2023-11-05 01:04:26 +0100

    Add error handling and update api calls for Start page.

--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -3,16 +3,18 @@ import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 import useBreakpoint, { DESKTOP } from '@hooks/useBreakpoint';
-import { fixUrl } from '@utils';
+import { fetchData } from '@utils/api';
 
 import Article from './Article/Article';
 import Event from './Event/Event';
 import { UserContext } from '@context/UserContext';
 import SEOHelmet from '@components/SEOHelmet/SEOHelmet';
 import ImageCarousel from '@components/Carousel/Carousel';
+import Loading from '@components/Spinner/Loading/Loading';
+import ErrorMessage from '@components/ErrorMessage/ErrorMessage';
 
 import styles from './Start.module.scss';
-import Loading from '@components/Spinner/Loading/Loading';
+import { fixUrl } from '@utils';
 
 interface Image {
   data: {
@@ -46,37 +48,10 @@ interface EventItem {
 }
 
 const Start = (): JSX.Element => {
-  const [newsItems, setNewsItems] = useState<NewsItem[]>([
-    {
-      attributes: {
-        title: '',
-        description: '',
-        linkTo: '',
-        image: {
-          data: {
-            id: 0,
-            attributes: {
-              formats: {
-                medium: {
-                  url: '',
-                },
-              },
-              url: '',
-            },
-          },
-        },
-      },
-    },
-  ]);
-  const [events, setEvents] = useState<EventItem[]>([
-    {
-      attributes: {
-        date: '',
-        title: '',
-        linkTo: '',
-      },
-    },
-  ]);
+  const [apiError, setApiError] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [newsItems, setNewsItems] = useState<NewsItem[] | null>(null);
+  const [events, setEvents] = useState<EventItem[]>([]);
   const { locale } = useContext(UserContext);
 
   const { t } = useTranslation();
@@ -112,27 +87,27 @@ const Start = (): JSX.Element => {
   ];
 
   useEffect(() => {
-    const getNewsItems = async (): Promise<void> => {
-      const response = await fetch(
-        fixUrl(
-          `/api/news-items?populate=*&pagination[page]=1&pagination[pageSize]=4&sort[createdAt]=DESC&locale=${locale}`,
-        ),
-      );
-      const apiData = await response.json();
-      setNewsItems(apiData.data);
-    };
-    const getEvents = async (): Promise<void> => {
-      const response = await fetch(
-        fixUrl(
-          `/api/events?populate=*&pagination[page]=1&pagination[pageSize]=3&sort[date]=ASC&locale=${locale}`,
-        ),
-      );
-      const apiData = await response.json();
-      setEvents(apiData.data);
-    };
-    void getNewsItems();
-    void getEvents();
+    void fetchData(
+      [
+        {
+          url: `/api/news-items?populate=*&pagination[page]=1&pagination[pageSize]=4&sort[createdAt]=DESC&locale=${locale}`,
+          setData: setNewsItems,
+          errorMessage: t('misc.apiErrors.newsItems'),
+        },
+        {
+          url: `/api/events?populate=*&pagination[page]=1&pagination[pageSize]=3&sort[date]=ASC&locale=${locale}`,
+          setData: setEvents,
+          errorMessage: t('misc.apiErrors.events'),
+        },
+      ],
+      setIsLoading,
+      setApiError,
+    );
   }, [locale]);
+
+  console.log('News items:', newsItems);
+
+  console.log('Error: ', apiError);
 
   return (
     <div className={styles.startContainer}>
@@ -140,18 +115,24 @@ const Start = (): JSX.Element => {
         title={t('helmet.start.title')}
         description={t('helmet.start.description')}
       />
-      {newsItems.length === 1 ? (
+      {isLoading ? (
         <Loading />
       ) : (
         <>
           <Article
             isMainArticle
-            title={newsItems[0].attributes.title}
-            description={newsItems[0].attributes.description}
-            linkTo={newsItems[0].attributes.linkTo}
+            title={newsItems !== null ? newsItems[0].attributes.title : ''}
+            description={
+              newsItems !== null ? newsItems[0].attributes.description : ''
+            }
+            linkTo={newsItems !== null ? newsItems[0].attributes.linkTo : ''}
             imageUrl={fixUrl(
-              newsItems[0].attributes.image.data.attributes.formats.medium.url,
+              newsItems !== null
+                ? newsItems[0].attributes.image.data.attributes.formats.medium
+                    .url
+                : '',
             )}
+            apiError={apiError}
           />
           <div className={styles.contentContainer}>
             {!isDesktop && (
@@ -165,38 +146,60 @@ const Start = (): JSX.Element => {
                 ></div>
               </div>
             )}
-            <div className={styles.eventsContainer}>
+            {!apiError.includes(t('misc.apiErrors.events')) && (
+              <div className={styles.eventsContainer}>
+                <h3 className={styles.upcomingHeader}>
+                  {t('start.upcoming')}
+                  {' ('}
+                  <Link to={'/gallery'} className={styles.moreEvents}>
+                    {t('start.moreEvents')}...
+                  </Link>
+                  {')'}
+                </h3>
+                <div className={styles.eventsGrid}>
+                  {events.map(({ attributes }, index) => (
+                    <Event
+                      key={index}
+                      date={attributes.date}
+                      title={attributes.title}
+                      linkTo={attributes.linkTo}
+                    />
+                  ))}
+                </div>
+              </div>
+            )}
+            <div className={styles.carouselWrapper}>
+              <ImageCarousel slides={slides} />
+            </div>
+            <div className={styles.articlesContainer}>
               <h3 className={styles.upcomingHeader}>
-                {t('start.upcoming')}
+                {t('start.otherNews')}
                 {' ('}
                 <Link to={'/gallery'} className={styles.moreEvents}>
                   {t('start.moreEvents')}...
                 </Link>
                 {')'}
               </h3>
-              <div className={styles.eventsGrid}>
-                {events.map(({ attributes }, index) => (
-                  <Event
-                    key={index}
-                    date={attributes.date}
-                    title={attributes.title}
-                    linkTo={attributes.linkTo}
-                  />
-                ))}
-              </div>
-            </div>
-            <div className={styles.carouselWrapper}>
-              <ImageCarousel slides={slides} />
-            </div>
-            <div className={styles.articlesGrid}>
-              {newsItems.slice(1, 4).map(({ attributes }, index) => (
-                <Article
-                  key={index}
-                  title={attributes.title}
-                  description={attributes.description}
-                  linkTo={attributes.linkTo}
+              {!apiError.includes(t('misc.apiErrors.newsItems')) ? (
+                <div className={styles.articlesGrid}>
+                  {newsItems
+                    ?.slice(1, 4)
+                    .map(({ attributes }, index) => (
+                      <Article
+                        key={index}
+                        title={attributes.title}
+                        description={attributes.description}
+                        linkTo={attributes.linkTo}
+                        apiError={apiError}
+                      />
+                    ))}
+                </div>
+              ) : (
+                <ErrorMessage
+                  identifier={t('misc.apiErrors.errorHeading')}
+                  errorMessage={apiError}
                 />
-              ))}
+              )}
             </div>
           </div>
         </>
