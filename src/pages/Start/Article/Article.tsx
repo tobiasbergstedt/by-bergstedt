eca6ec60dac5e2ae341ed6dafb4441041391db4c
Tobias Bergstedt <info@tobiasbergstedt.se>
2023-11-14 18:16:32 +0100

    Edit Article design and layout.

--- src/pages/Start/Article/Article.tsx
+++ src/pages/Start/Article/Article.tsx
@@ -1,8 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import clsx from 'clsx';
 
-import useBreakpoint, { DESKTOP } from '@hooks/useBreakpoint';
-
 import Button from '@components/Button/Button';
 import ErrorMessage from '@components/ErrorMessage/ErrorMessage';
 
@@ -27,68 +25,75 @@ const Article = ({
   imageAlt,
   apiError,
 }: ArticleProps): JSX.Element => {
-  const breakpoint = useBreakpoint();
-  const isDesktop = breakpoint === DESKTOP;
   const { t } = useTranslation();
 
   return (
-    <article className={styles.mainArticle}>
-      {!apiError.includes(t('misc.apiErrors.newsItems')) ? (
-        <section
-          className={clsx(styles.mainContent, {
-            [styles.gridContent]: !(isMainArticle ?? false),
-          })}
-        >
-          {imageUrl != null && (
-            <div className={styles.mainImageWrapper}>
-              <a href={linkTo}>
-                <img
-                  src={imageUrl}
-                  alt={imageAlt}
-                  className={styles.mainImage}
-                />
-              </a>
-            </div>
-          )}
-          {isMainArticle === true ? (
-            <a href={linkTo}>
-              <h1 className={styles.mainHeading}>{title}</h1>
-            </a>
-          ) : (
-            <a href={linkTo}>
-              <h2 className={clsx(styles.mainHeading, styles.smallerHeading)}>
-                {title}
-              </h2>
-            </a>
-          )}
+    <div className={styles.articleContainer}>
+      <article
+        className={clsx(styles.mainArticle, {
+          [styles.isMainArticle]: isMainArticle,
+        })}
+      >
+        {!apiError.includes(t('misc.apiErrors.newsItems')) ? (
+          <>
+            <section
+              className={clsx(styles.mainContent, {
+                [styles.gridContent]: !(isMainArticle ?? false),
+              })}
+            >
+              {isMainArticle === true ? (
+                <a href={linkTo}>
+                  <h1 className={styles.mainHeading}>{title}</h1>
+                </a>
+              ) : (
+                <a href={linkTo}>
+                  <h2
+                    className={clsx(styles.mainHeading, styles.smallerHeading)}
+                  >
+                    {title}
+                  </h2>
+                </a>
+              )}
 
-          <p
-            className={clsx(styles.mainParagraph, {
-              [styles.smallParagraph]: !(isMainArticle ?? false),
-            })}
-          >
-            {description}
-          </p>
-          <Button to={linkTo}>{t('start.readMore')}</Button>
-        </section>
-      ) : (
-        <ErrorMessage
-          identifier={t('misc.apiErrors.errorHeading')}
-          errorMessage={apiError}
-        />
+              <p
+                className={clsx(styles.mainParagraph, {
+                  [styles.smallParagraph]: !(isMainArticle ?? false),
+                })}
+              >
+                {description}
+              </p>
+              {isMainArticle === true && (
+                <Button to={linkTo}>{t('start.readMore')}</Button>
+              )}
+            </section>
+            <section
+              className={clsx(styles.imageSection, {
+                [styles.isMainArticle]: isMainArticle,
+              })}
+            >
+              {imageUrl != null && (
+                <a
+                  href={linkTo}
+                  className={clsx(styles.mainImageWrapper, {
+                    [styles.mainArticleImageWrapper]: isMainArticle,
+                  })}
+                >
+                  <img src={imageUrl} alt={imageAlt} />
+                </a>
+              )}
+            </section>
+          </>
+        ) : (
+          <ErrorMessage
+            identifier={t('misc.apiErrors.errorHeading')}
+            errorMessage={apiError}
+          />
+        )}
+      </article>
+      {isMainArticle === false && (
+        <Button to={linkTo}>{t('start.readMore')}</Button>
       )}
-      {isDesktop && isMainArticle === true && (
-        <div className={styles.instaWrapper}>
-          <div
-            style={{
-              width: '500px',
-              height: '500px',
-              backgroundColor: 'red',
-            }}
-          ></div>
-        </div>
-      )}
-    </article>
+    </div>
   );
 };
 
